c0906df2ee633f1e25a959585efd0c01f828b0ef
Gabriel Marini <gabrielmarini12@icloud.com>
2024-06-25 21:46:35 -0300

    Exibindo apenas os carros cadastrados no dashboard.

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -4,7 +4,14 @@ import { DashboardHeader } from "../../components/panelheader";
 
 import { FiTrash2 } from "react-icons/fi";
 
-import { collection, getDocs, where, query } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  where,
+  query,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
 import { db } from "../../services/firebaseConnection";
 import { AuthContext } from "../../contexts/AuthContext";
 
@@ -61,35 +68,47 @@ export function Dashboard() {
     loadCars();
   }, [user]);
 
+  async function handleDeleteCar(id: string) {
+    const docRef = doc(db, "cars", id);
+    await deleteDoc(docRef);
+    setCars(cars.filter((car) => car.id !== id));
+  }
+
   return (
     <Container>
       <DashboardHeader />
 
       <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-        <section className="w-full bg-white rounded-lg relative">
-          <button
-            onClick={() => {}}
-            className="absolute bg-white w-14 h-14 rounded-full flex items-center justify-center right-2 top-2 drop-shadow"
-          >
-            <FiTrash2 size={26} color="#000" />
-          </button>
-          <img
-            className="w-full rounded-lg mb-2 max-h-70"
-            src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2024/202406/20240615/porsche-panamera-2-9-v6-ehybrid-4s-pdk-wmimagem01103176639.webp?s=fill&w=552&h=414&q=60"
-          />
-          <p className="font-bold mt-1 px-2 mb-2">Porshe Panamera</p>
+        {cars.map((car) => (
+          <section key={car.id} className="w-full bg-white rounded-lg relative">
+            <button
+              onClick={() => handleDeleteCar(car.id)}
+              className="absolute bg-white w-14 h-14 rounded-full flex items-center justify-center right-2 top-2 drop-shadow"
+            >
+              <FiTrash2 size={26} color="#000" />
+            </button>
+            <img
+              className="w-full rounded-lg mb-2 max-h-70"
+              src={car.images[0].url}
+            />
+            <p className="font-bold mt-1 px-2 mb-2">{car.name}</p>
 
-          <div className="flex flex-col px-2">
-            <span className="text-zinc-700">Ano 2022/2023 | 230.000 km</span>
-            <strong className="text-black font-bold mt-4">R$ 150.000</strong>
-          </div>
+            <div className="flex flex-col px-2">
+              <span className="text-zinc-700">
+                Ano {car.year} | {car.km} km
+              </span>
+              <strong className="text-black font-bold mt-4">
+                R$ {car.price}
+              </strong>
+            </div>
 
-          <div className="w-full h-px bg-slate-200 my-2"></div>
+            <div className="w-full h-px bg-slate-200 my-2"></div>
 
-          <div className="px-2 pb-2">
-            <span className="text-black">Canoas - RS</span>
-          </div>
-        </section>
+            <div className="px-2 pb-2">
+              <span className="text-black">{car.city}</span>
+            </div>
+          </section>
+        ))}
       </main>
     </Container>
   );
